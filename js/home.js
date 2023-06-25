169ca505132423f50588eaaeb17b3735380dd9ca
Marek <sincereakram@gmail.com>
2023-06-25 20:20:00 +0300

    still trying

--- js/home.js
+++ js/home.js
@@ -5,7 +5,7 @@ function startAnimation() {
   animationInterval = setInterval(function () {
     image.style.transform =
       "translate(" + getRandomOffset() + "px, " + getRandomOffset() + "px)";
-  }, 1000);
+  }, 5000);
 }
 
 function stopAnimation() {
@@ -23,7 +23,22 @@ let count = 0;
 const interval = setInterval(function () {
   count++;
   countElement.textContent = count.toLocaleString();
-  if (count === 9999) {
+  if (count === 1234) {
     clearInterval(interval);
   }
 }, 1);
+
+const druidImage = document.getElementById('druid-image');
+
+druidImage.addEventListener('mouseover', () => {
+  druidImage.classList.add('darker');
+});
+
+druidImage.addEventListener('mouseout', () => {
+  druidImage.classList.remove('darker');
+  druidImage.classList.add('lighter');
+});
+
+druidImage.addEventListener('transitionend', () => {
+  druidImage.classList.remove('lighter');
+});
